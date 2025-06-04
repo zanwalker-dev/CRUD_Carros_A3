cd00012ca86b87b29faa2a4626f8d238a77a0e8e
Zan Rocha <andre.rocha2302@gmail.com>
2025-06-04 19:16:14 -0300

    refactor: Sedan refactored without CRUD methods

--- src/Sedan.java
+++ src/Sedan.java
@@ -1,15 +1,12 @@
 import java.time.Year;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Sedan extends Carro {
     private double preco;
     private double espacoPortaMala;
-    private static List<Sedan> sedans = new ArrayList<>();
 
     public Sedan() {}
 
-    public Sedan(String placa, String modelo, String marca, int ano, int quilometragem,double preco, double espacoPortaMala) {
+    public Sedan(String placa, String modelo, String marca, int ano, int quilometragem, double preco, double espacoPortaMala) {
         this.placa = placa;
         this.modelo = modelo;
         this.marca = marca;
@@ -36,40 +33,16 @@ public class Sedan extends Carro {
         this.espacoPortaMala = espacoPortaMala;
     }
 
-    // Métodos CRUD
     @Override
     public void cadastrarVeiculo() {
-        if (Carro.placaExistente(this.placa)) {
-            throw new IllegalArgumentException("Placa já cadastrada");
-        }
-        carros.add(this);
-        sedans.add(this);
-    }
-
-    public static boolean atualizarSedan(String placa, double novoPreco, double novoEspacoPortaMala) {
-        for (Sedan sedan : sedans) {
-            if (sedan.getPlaca().equals(placa)) {
-                sedan.setPreco(novoPreco);
-                sedan.setEspacoPortaMala(novoEspacoPortaMala);
-                return true;
-            }
-        }
-        return false;
+        GerenciadorVeiculos.adicionar(this);
     }
 
     @Override
     public double valorAtual() {
         int anos = Year.now().getValue() - this.ano;
-        // Desvalorização por ano (Sedan desvaloriza médio. Primeiro ano:7%, quinto ano: 20%, teto: 30%)
         double percentualAno = Math.min(0.30, 0.07 + (anos * 0.026));
-        // Desvalorização menor por KM (0.25% a cada 1.000 km)
         double percentualKm = Math.min(0.20, (this.quilometragem / 1000) * 0.0025);
-
         return this.preco * (1 - Math.min(0.50, percentualAno + percentualKm));
-    }
-
-
-    public static List<Sedan> getTodosSedans() {
-        return new ArrayList<>(sedans);
     }
 }
